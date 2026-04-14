42fff30c1a2436f465c7bb1966b675ee8808f68a
Stefania mendez_12 <stefaniamendez124@unisangil.edu.co>
2026-04-13 20:55:53 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// rutas
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () =>{
+    console.log('ejecutando en el puerto 3000');
+});
